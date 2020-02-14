c26664b0f48538f3c7278fb59d8affe9307eef0f
drewgillson <drew.gillson@gmail.com>
2020-02-13 22:45:36 -0700

    Update run_tests.py

--- .github/actions/run_tests.py
+++ .github/actions/run_tests.py
@@ -4,7 +4,6 @@ import sys
 sdk = client.setup()
 
 def main():
-    print(sys.argv)
     broken_content_prod = sdk.content_validation().content_with_errors
     
     """Enter dev mode """
@@ -12,8 +11,8 @@ def main():
     
     """ Check out this branch """
     branch = sys.argv[1].replace("refs/heads/","")
-    sdk.update_git_branch(project_id="my_fruit_basket2", body=models.WriteGitBranch(name=branch)
-
+    resp = sdk.update_git_branch(project_id="my_fruit_basket2", body=models.WriteGitBranch(name=branch)
+    print(resp)
     broken_content_dev = sdk.content_validation().content_with_errors
 
     # Assert no new errors introduced in dev branch
