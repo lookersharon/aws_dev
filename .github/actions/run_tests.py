02efe4bd66dee4e9e61ee956acd8c17b093e11c9
drewgillson <drew.gillson@gmail.com>
2020-02-13 22:59:54 -0700

    Update run_tests.py

--- .github/actions/run_tests.py
+++ .github/actions/run_tests.py
@@ -11,12 +11,16 @@ def main():
     
     """ Check out this branch """
     branch = sys.argv[1].replace("refs/heads/","")
-    resp = sdk.update_git_branch(project_id="my_fruit_basket2", body=models.WriteGitBranch(name=branch))
-    print(resp)
+    resp = sdk.update_git_branch(project_id=sys.argv[2], body=models.WriteGitBranch(name=branch))
     broken_content_dev = sdk.content_validation().content_with_errors
 
     # Assert no new errors introduced in dev branch
     assert len(broken_content_dev) - len(broken_content_prod) <= 0, """
         Uh oh. you just introduced a new content error!"""
+
+    tests = sdk.all_lookml_tests(project_id=sys.argv[2])
+    for test in tests:
+        resp = sdk.run_lookml_test(project_id=sys.argv[2], test=test.name, model=test.model_name)
+        assert resp[0].assertions_failed == 0, resp[0].errors
 
 main()
