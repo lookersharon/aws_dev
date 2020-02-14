d7b86b33ab00680591b93b5eedef607309b53cc7
drewgillson <drew.gillson@gmail.com>
2020-02-13 23:18:57 -0700

    Update run_data_tests_and_content_validator.py

--- .github/actions/run_data_tests_and_content_validator.py
+++ .github/actions/run_data_tests_and_content_validator.py
@@ -21,6 +21,10 @@ def main():
     tests = sdk.all_lookml_tests(project_id=sys.argv[2])
     for test in tests:
         resp = sdk.run_lookml_test(project_id=sys.argv[2], test=test.name, model=test.model_name)
+        if resp[0].errors:
+            with fileinput.FileInput(resp[0].errors[0].file_path.replace(sys.argv[2] + "/",""), inplace=True) as file:
+                for line in file:
+                    print(line.replace("test: " + resp[0].test_name, "# This test failed on " + now.strftime("%m/%d/%Y %H:%M:%S") + "!\ntest: " + resp[0].test_name), end='')
         assert resp[0].assertions_failed == 0, resp[0].errors
 
 main()
