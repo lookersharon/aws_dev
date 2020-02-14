013f136233ab5e014b749f941150f25502b95011
drewgillson <drew.gillson@gmail.com>
2020-02-13 23:43:05 -0700

    Update run_data_tests_and_content_validator.py

--- .github/actions/run_data_tests_and_content_validator.py
+++ .github/actions/run_data_tests_and_content_validator.py
@@ -26,7 +26,7 @@ def main():
         if resp[0].errors:
             with fileinput.FileInput(resp[0].errors[0].file_path.replace(sys.argv[2] + "/",""), inplace=True) as file:
                 for line in file:
-                    print(line.replace("test: " + resp[0].test_name, "# This test failed on " + now.strftime("%m/%d/%Y %H:%M:%S") + "!\ntest: " + resp[0].test_name), end='')
+                    print(line.replace("test: " + resp[0].test_name, "# This test failed on " + datetime.now().strftime("%m/%d/%Y %H:%M:%S") + "!\ntest: " + resp[0].test_name), end='')
         assert resp[0].assertions_failed == 0, resp[0].errors
 
 main()
